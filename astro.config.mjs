9eaa3fd6ec345309eb1fd6f38f551569764d1e02
Joshua Sosa Cruz <joshua.developer21@gmail.com>
2023-06-18 10:08:00 -0500

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce',
+  site: "https://creativetimofficial.github.io",
+  base: "/astro-ecommerce",
+  output: "server",
+  adapter: vercel(),
 });
